5af3c7233ba7aa02c020dd73a8087a68e19e3955
MykolaPixode <m.andrieiev@pixode.com>
2023-09-15 20:26:59 +0300

    Add Quest log

---
+++ Assets/MyNovel/Scripts/QuestLog/QuestLogCommand.cs
@@ -0,0 +1,18 @@
+﻿using Naninovel;
+
+namespace Assets.MyNovel.Scripts.QuestLog
+{
+    [CommandAlias("quest")]
+    public class QuestLogCommand : Command
+    {
+        [RequiredParameter]
+        public StringParameter Summary;
+
+        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+        {
+            Engine.GetService<QuestLogService>().UpdateQuestLog(Summary);
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
---
+++ Assets/MyNovel/Scripts/QuestLog/QuestLogService.cs
@@ -0,0 +1,24 @@
+﻿using Naninovel;
+using UnityEngine.Events;
+
+namespace Assets.MyNovel.Scripts.QuestLog
+{
+    [InitializeAtRuntime]
+    public class QuestLogService : IEngineService
+    {
+        public event UnityAction<string> QuestLogUpdated;
+        public string LastQuestLog { get; private set; } = "";
+
+        public UniTask InitializeServiceAsync() =>
+            UniTask.CompletedTask;
+        public void ResetService() { }
+        public void DestroyService() { }
+
+        public void UpdateQuestLog(string description)
+        {
+            LastQuestLog = description;
+
+            QuestLogUpdated?.Invoke(LastQuestLog);
+        }
+    }
+}
